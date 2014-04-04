46ac4e54d77794882206280f2bc8fbe1ae52707f
Bert Poller <bpoller@ekito.fr>
2014-04-04 17:50:57 +0200

    Updated copy logic

--- edf_reader/edf_reader.h
+++ edf_reader/edf_reader.h
@@ -1,12 +1,29 @@
 #include <Arduino.h>
 
-// offPeakIndex - the index when a value shall be interpreted as 'OffPeak'.
-// offPeakIndex = 0 means all values are off peak (HC)
-// offPeakIndex = 10 means all values are peak (HP)
-// offPeakIndex = 3 means values 3 and onwards are off peak (HC). Values 0, 1 and 2 are peak HP.
+/*
+
+transitionIndex indicates the index of values where a transition from off-peak to 
+peak tariff or vice-versa happened. Default value is -1, meaning no transition took 
+place during the measurement intervall.
+
+Transition Direction 0 means downwards -> from peak tariff to off-peak tariff.
+                     1 means upwards   -> from off peak to peak tariff.
+                     
+If transition index is -1, then transition direction indicates the current 
+tariff that operates (0: off peak; 1: peak)
+
+*/
+
 struct Reading {
-  short offPeakIndex ;
+  short transitionIndex;
+  short transitionDirection;
   short values[10];
+};
+
+struct Values {
+  int hc;
+  int hp;
+  short isPeak;
 };
  
 void sendData(Reading reading, int position);
--- edf_reader/edf_reader.ino
+++ edf_reader/edf_reader.ino
@@ -5,12 +5,13 @@
 Akeru_ akeru;
 
 Reading reading = {
-  10,{
-    0,0,0,0,0,0,0,0,0,0      }
+  0, 0,{
+    0,0,0,0,0,0,0,0,0,0        }
 };
 
+Values previousValues = {0,0,0};
+
 int position = 0;
-int counter = 0;
 
 unsigned long time;
 
@@ -18,14 +19,14 @@ void setup() {
   pinMode(13, OUTPUT);
   akeru.begin();
   Serial.begin(1200);
-  time = millis();
   delay(1000);
   Serial.println("Ready to go...");
+  time = millis();
 }
 
 void loop() {
   sendData(updateReading(reading, position), position);
-  position = tick(position, 1000);
+  position = tick(position, 5000);
 }
 
 // Send data to Sigfox network
@@ -34,7 +35,7 @@ void sendData(Reading reading, int position){
   {
     digitalWrite(13, HIGH);
     Serial.println("Sending...");
-    akeru.send(&reading, sizeof(reading));
+    //akeru.send(&reading, sizeof(reading));
     Serial.println("done");
     digitalWrite(13, LOW); 
   }
@@ -42,44 +43,51 @@ void sendData(Reading reading, int position){
 
 Reading updateReading(Reading reading, int position){
 
+  boolean validFrame = false;
   char frame[512] = "";
-  boolean isOffPeak = false;
   short counter = 0;
+  Values values = {0, 0, false};
 
-  while (!isValid(frame)){
+  Serial.println("update reading");
+
+  while (!validFrame){
     waitForFrameBeginning();
     receiveFrame(frame);
-    isOffPeak = isOffPeakHours(frame);
-    short counter = extractCount(frame, isOffPeak);
+    validFrame = extractValues(frame,values);
+    printFrame(frame);
   }
 
-  return copyInto(reading, position, isOffPeak,  counter);
+  return copyInto(reading, position, values);
 }
 
-boolean isOffPeakHours(char frame[]){
+boolean extractValues(char frame[], Values values){
+
+  
+  
   return true;
 }
 
-short extractCount(char frame[], boolean isOffPeak)
-{
-  return 0;
+void printFrame(char frame[]){
+  for (int i=0; i < strlen(frame); i++){
+    Serial.write(frame[i]);
+  }
 }
 
-Reading copyInto(Reading reading, int position, boolean isOffPeak, short counter){
-  if(isOffPeak){
-    reading.offPeakIndex = position;
+Reading copyInto(Reading reading, int position, Values currentValues){
+  if (currentValues.isPeak != previousValues.isPeak)  {
+    reading.transitionIndex = position;
+    reading.transitionDirection = currentValues.isPeak;
   }
-  reading.values[position] = counter;
+  reading.values[position] = (currentValues.isPeak==1)? currentValues.hp : currentValues.hc;
+  previousValues = currentValues;
   return reading;
-}
-
-boolean isValid(char frame[]){
-  return true;
 }
 
 // Fast forward to the beginning of the next frame (0x02)
 void waitForFrameBeginning(){
   char receivedChar ='\0';
+
+  Serial.println("wait for beginning...");
   while(receivedChar != 0x02){
     if (Serial.available()) {
       receivedChar = Serial.read() & 0x7F;
@@ -90,6 +98,8 @@ void waitForFrameBeginning(){
 void receiveFrame(char frame[]){
   int i=0;
   char receivedChar ='\0';
+
+  Serial.println("receiveFrame");
 
   while(receivedChar != 0x03) { 
     if (Serial.available()) {
@@ -103,7 +113,11 @@ void receiveFrame(char frame[]){
 int tick(int position, int period){
   position = (position+1) % 10;
   unsigned long cycleTime = millis() - time;
+  Serial.print("Cycle time:");
+  Serial.println(cycleTime);
+  
   delay(period-cycleTime);
   time = millis();
   return position; 
 }
+
